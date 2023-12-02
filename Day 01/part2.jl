ad0aa63535a55b3da0a21a477dd98a853983b96e
Kacper W <48856951+n0tcnj1eck4h@users.noreply.github.com>
2023-12-02 02:15:38 +0100

    this a bit more awesome

--- Day 01/part2.jl
+++ Day 01/part2.jl
@@ -1,5 +1,8 @@
 sum = 0
-digits = [ "1" "2" "3" "4" "5" "6" "7" "8" "9" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine" ]
+digits = [ 
+  "1" "2" "3" "4" "5" "6" "7" "8" "9" 
+  "one" "two" "three" "four" "five" "six" "seven" "eight" "nine" 
+]
 
 function ermm(f, c, line)
   finds = filter(x -> !isnothing(x), f.(digits, line))
@@ -8,19 +11,12 @@ function ermm(f, c, line)
   return finds[v[2]]
 end
 
-while !eof(stdin::IO) line = readline()
-  first_digit_range = ermm(findfirst, findmin, line) 
-  last_digit_range  = ermm(findlast,  findmax, line)
-  println(line[first_digit_range], " ", line[last_digit_range])
-
-  idx = findfirst(isequal(line[first_digit_range]), digits)[2]
-  idx = (idx-1) % 9 + 1
-  global sum += idx * 10
-
-  idx = findfirst(isequal(line[last_digit_range]), digits)[2]
-  idx = (idx-1) % 9 + 1
-  global sum += idx 
-  
+while !eof(stdin::IO) 
+  line = readline()
+  first_digit = line[ermm(findfirst, findmin, line)] 
+  last_digit  = line[ermm(findlast,  findmax, line)]
+  global sum += findfirst(isequal(first_digit), digits)[2] * 10
+  global sum += findfirst(isequal(last_digit),  digits)[2]
 end
 
 println(sum)
